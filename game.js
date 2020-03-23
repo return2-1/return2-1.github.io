c2c4b62921d290cc8513fe5e4bffe050b1f84fe5
return2-1 <61298950+return2-1@users.noreply.github.com>
2020-03-23 23:01:14 +0800

    第三个版本

--- game.js
+++ game.js
@@ -1,192 +1,175 @@
-//计分板
-var board = {
-  dom: document.getElementById("score"),
-  maxLost: 3, //最大丢失量
-  lost: 0, //当前丢失了多少个
-  score: 0, //当前分数
-  render: function() {
-    //显示
-    this.dom.innerHTML =
-      "<p>得分：" +
-      this.score +
-      "</p><p>丢失：" +
-      this.lost +
-      " / " +
-      this.maxLost +
-      "</p>";
-  },
-  //增加一个丢失数
-  addLost: function() {
-    if (this.lost === this.maxLost) {
-      return; //游戏已经结束了
-    }
-    this.lost++;
-    if (this.lost === this.maxLost) {
-      //丢失量达到最大
-      game.gameOver();
-    }
-    this.render();
-  },
-  reset: function() {
-    this.lost = 0;
-    this.score = 0;
-    this.render();
-  },
-  //增加得分
-  addScore: function(number) {
-    if (this.lost === this.maxLost) {
-      //已经结束了
-      return;
-    }
-    this.score += number;
-    this.render();
-  }
-};
 
-board.render();
+var oo = (function(){
+	var grade = {
+gradeboard:document.getElementsByClassName("gradeboard")[0],
+over : document.getElementsByClassName("over")[0],
+score:0,
+lostletter:0,
+maxlost:3,
+//成绩内容函数
+gradecontent:function(){
+     
+	this.gradeboard.innerHTML = `<p>得分：${this.score}</p>
+<p>丢失：${this.lostletter}/${this.maxlost}</p>`
+},
+//得分功能
+addScore:function(){
+if(this.lostletter >= this.maxlost){return;}
+	this.score += 10;
+	this.gradecontent();
+},
 
-var letters = []; //目前的所有字母，一个字母就是一个对象
-//字母的容器
-var divContainer = document.getElementById("letter-container");
-/**
- * 产生一个字母对象，并将其加入到数组中
- */
-function createLetter() {
-  //创建img元素
-  var img = document.createElement("img");
-  img.className = "letter";
-
-  divContainer.appendChild(img);
-
-  //设置src路径
-  var charNumber = getRandom(65, 65 + 26); //字母的随机ASCII码
-  var char = String.fromCharCode(charNumber);
-  img.src = "img/letter/" + char + ".png";
-
-  //left随机
-  var left = getRandom(0, divContainer.clientWidth - img.width);
-  img.style.left = left + "px";
-
-  var letter = {
-    dom: img,
-    char: char,
-    left: left,
-    top: -img.height, //初始的top值
-    speed: getRandom(100, 500), //速度: 像素/秒
-    render: function() {
-      this.dom.style.top = this.top + "px";
-    },
-    // 没调用一次该方法，字母移动一段距离
-    // duration是经过的时间: 秒
-    move: function(duration) {
-      var dis = duration * this.speed; //计算距离
-      this.top += dis;
-      this.render();
-    },
-    kill: function() {
-      //自杀
-      // 从数组中移除
-      var index = letters.indexOf(this); //找到字母在数组中的下标
-      if (index >= 0) {
-        letters.splice(index, 1);
-      }
-      // 移除dom元素
-      this.dom.remove();
-    }
-  };
-
-  letter.render();
-
-  letters.push(letter);
+//丢失字母时计数功能
+lostLetter:function(){
+	this.lostletter += 1;
+	if(this.lostletter > this.maxlost){return;};
+	if(this.lostletter == this.maxlost){this.over.style.display = "block";go.addLock = 1;};
+	this.gradecontent();
+},
 }
+grade.gradecontent();
 
-// 根据最小值和最大值产生一个随机整数(不包含最大值)
-function getRandom(min, max) {
-  // Math.random()   0~1
-  // Math.random() * (max - min)    0 ~ (max - min)
-  // Math.random() * (max - min) + min    min ~ max
-  return Math.floor(Math.random() * (max - min) + min);
+
+
+var letterImgs = document.getElementsByClassName("letter-container")[0];
+var letters = []
+var duration = 0.016;
+var imgSpeed = 50;
+//生成一个随机的字母 ，并且将其以一个变量的格式添加进数组里，这个变量里又属于对自身的操作的方法。
+ function createLetter(){
+    var img = document.createElement("img");
+	img.className = "letter";
+	var char = randomLetter();
+    img.src = "./img/letter/" + char + ".png";
+    letterImgs.appendChild(img);
+    var lef = random(0,letterImgs.clientWidth - img.width);//为什么不能用window.getComputedStyle(letterImgs,null).width方法。
+    img.style.left = lef + "px";
+    
+    
+   //随机数功能
+  function random(min,max){
+return Math.floor(Math.random()*(max - min)) + min; 
+};
+
+//随机大写字母功能
+  function randomLetter(){
+var randomNum = random(65,65+26) //随机出A-Z的asc码，
+var Letter = String.fromCharCode(randomNum);//随机出相应的大写字母
+return Letter;
+};
+ 
+var letter = {
+dom: img,
+char: char,
+speed: random(100,300),
+top: -img.offsetHeight,
+render : function (){
+ this.dom.style.top = this.top + "px";
+},
+move : function (duration){
+ var di = duration * this.speed;//duration是时间，单位：秒。每秒移动this.speed的距离。
+ this.top += di;
+ this.render();
+},
+kill : function(){
+
+     var index = letters.indexOf(this);//可以选出该对象在数组中的位置。
+     if(index >= 0){
+     	letters.splice(index,1);//从数组中移除该对象
+     };
+      	this.dom.remove();//删除自身。
+      
+},
+
+};
+letter.render(); 
+letters.push(letter);
+};
+
+//组装游戏功能
+var go ={
+addLock : null,
+fallLock : null,
+//持续生产字母功能
+add: function (){
+	var self =this;
+    
+   var addletter = setInterval(function(){
+     if(self.addLock){clearInterval(addletter);}
+   	createLetter();},650);
+},
+
+//字母下落功能
+fall : function (){
+var self = this;
+
+setInterval(function(){
+	if(self.addLock){return;};
+		for(var i = 0; i < letters.length; i ++){
+			var letter = letters[i];
+            if(parseInt( window.getComputedStyle(letter.dom,null).top) >= parseInt( window.getComputedStyle(letterImgs,null).height)){
+		        letter.kill();
+		       i --;
+	            grade.lostLetter(); //丢失加一；
+	};
+	letter.move(duration);
+};
+			
+			
+		
+	},duration * 1000);
+
+},
+//字母消失功能
+Kill : function (){
+for(var i = 0; i < letters.length; i ++){
+	var letter = letters[i];
+	};
+},
+
+start : function (){
+	this.add();
+	this.fall();
+    },
+};
+var rebirth = document.getElementsByClassName("rebirth")[0];
+var startButton = document.getElementsByClassName("button")[0];//将游戏开关放入这个点击事件内。
+var gameMethod = function(){
+	startButton.addEventListener ('click',function(){
+	go.start();
+	startButton.style.display = "none";
+},false);
+rebirth.addEventListener('click',function(){
+for(var i = 0; i < letters.length; i++){
+	var letter = letters[i];
+	if(letter.top >= -parseInt(window.getComputedStyle(letter.dom,null).height)){
+		letter.kill();
+		i --;
+	}
 }
+grade.score = 0;
+grade.lostletter = 0;
+grade.over.style.display = "none";
+grade.gradecontent();
+go.addLock = false;
+go.add();
 
-//游戏对象，统筹规划
-var game = {
-  timerProduce: null, //自动产生字母的timerid
-  timerMove: null, //自动移动的timerid
-  isOver: false,
-  //自动的，不断的，产生字母
-  startProduce: function() {
-    if (this.timerProduce) {
-      return; //正在产生中，什么也不做
-    }
-    this.timerProduce = setInterval(createLetter, 500);
-  },
-  //停止自动产生字母
-  stopProduce: function() {
-    clearInterval(this.timerProduce);
-    this.timerProduce = null;
-  },
-  //开始不断的移动所有字母
-  startMove: function() {
-    if (this.timerMove) {
-      return;
-    }
-    var duration = 0.016; //间隔时间，秒
-    this.timerMove = setInterval(function() {
-      for (var i = 0; i < letters.length; i++) {
-        var letter = letters[i]; //要移动的字母
-        letter.move(duration);
-        //判断该字母是不是可以消除了
-        if (letter.top >= divContainer.clientHeight) {
-          letter.kill();
-          i--;
-          //丢失
-          board.addLost();
-        }
-      }
-    }, duration * 1000);
-  },
-  //停止移动所有字母
-  stopMove: function() {
-    clearInterval(this.timerMove);
-    this.timerMove = null;
-  },
-  gameOver: function() {
-    this.stopMove();
-    this.stopProduce();
-    document.getElementById("over").style.display = "block";
-    this.isOver = true;
-  },
-  restart: function() {
-    //清空字母
-    for (var i = 0; i < letters.length; i++) {
-      var letter = letters[i];
-      letter.kill();
-      i--;
-    }
-    this.startMove();
-    this.startProduce();
-    board.reset();
-    this.isOver = false;
-    document.getElementById("over").style.display = "none";
-  }
+	
+})
+setTimeout(go.fall,10)
+//注册键盘监听事件
+window.onkeydown = function(e){
+	if(go.addLock){return;};
+	for(var i = 0; i < letters.length; i ++){
+		var letter = letters[i];
+		
+if( e.key.toUpperCase() == letter.char){
+	letter.kill();//删除自身
+	grade.addScore();//分数++。
 };
 
-game.startProduce();
-game.startMove();
-
-//注册事件
-window.onkeydown = function(e) {
-  if (game.isOver) {
-    return;
-  }
-  var key = e.key.toUpperCase();
-  //匹配
-  for (var i = 0; i < letters.length; i++) {
-    var letter = letters[i];
-    if (letter.char === key) {
-      letter.kill();
-      board.addScore(10);
-      return; //只移除一个
-    }
-  }
-};
+}
+}}
+return gameMethod;
+}());
+oo();
